8345020b997f3e34c58b5cbf3d25b031fffcbecd
iraklitsikhelishvilii <11cixela11@gmail.com>
2024-10-20 23:26:13 +0400

    js

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1 ამოცანა
+let x = 12;
+console.log(x * x);
+if (x * x > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+// 2 ამოცანა
+let number1 = 10;
+if (number1 > 50 && number1 < 100) {
+  console.log("შუალედში მოხვდი");
+} else {
+  console.log("კიდევ სცადე");
+}
+// 3 ამოცანა
+let number2 = 0;
+if (number2 <= 0) {
+  console.log("რიცხვი ნაკლებია ან ტოლია ნულის");
+} else {
+  console.log("დადებითია");
+}
+// 4 ამოცანა
+let სიგრძე = 10;
+let სიგანე = 5;
+console.log(2 * (სიგრძე + სიგანე));
+if (2 * (სიგრძე + სიგანე) > 50) {
+  console.log("პერიმეტრი მეტია 50-ზე");
+} else {
+  console.log("პერიმეტრი არარის მეტი 50-ზე");
+}
